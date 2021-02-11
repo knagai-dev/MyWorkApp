56029f7ffad8fc79e8a9ac95f27f5cf137644961
kaito <kaito.develop@gmail.com>
2021-02-11 20:39:50 +0900

    Add share func

--- App.tsx
+++ App.tsx
@@ -1,67 +1,31 @@
 import React from 'react';
-import { Alert, Text, View, StyleSheet, ViewStyle } from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Share} from 'react-native';
 
-const lightThema = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
-    color: 'black',
   },
-  label: {
-    backgroundColor: 'white',
-    color: 'black',
-  },
-})
-
-const darkThema = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-    color: 'white',
-  },
-  label: {
-    backgroundColor: 'black',
-    color: 'white',
-  },
-})
-
-type Thema = typeof lightThema | typeof darkThema;
-interface Context {
-  thema: Thema;
-  setThema: (newThema: Thema) => void;
-}
-
-const ThemaContext = React.createContext<Context>({
-  thema: lightThema,
-  setThema: (_: typeof lightThema | typeof darkThema) => {},
 });
 
+async function onShare() {
+  try {
+    await Share.share({
+      title: 'タイトル',
+      message: '共有したいメッセージ',
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export default function App() {
-  const [thema, setThema] = React.useState(lightThema);
-
   return (
-    <ThemaContext.Provider value={{thema, setThema}}>
-      <View style={thema.container}>
-        <Text onPress={() => {
-          setThema(thema === lightThema ? darkThema : lightThema)
-        }}
-        style={thema.label}>
-          切り替え
-        </Text>
-        <Profile name="knagai" />
-      </View>
-    </ThemaContext.Provider>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onShare}>
+        <Text>Share</Text>
+      </TouchableOpacity>
+    </View>
   );
-}
-
-interface Props {
-  name: string
-}
-function Profile(props: Props) {
-  const {thema} = React.useContext(ThemaContext)
-  return <Text style={thema.label}>{props.name}</Text>
 }
